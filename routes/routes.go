16a025544af3a47dcae0d7c553c7ec419ab6d2a6
3b00d09 <bd315977@gmail.com>
2023-09-23 15:15:37 +0100

    forms for login and register

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func SetupRoutes() *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/", handler.HandleIndexRoute)
-	r.Post("/login", handler.HandleLoginRoute)
+	r.Get("/login", handler.HandleLoginRoute)
 	r.Get("/register", handler.HandleRegisterRoute)
 
 	return r
