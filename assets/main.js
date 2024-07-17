8baff1f1eab96f4d803d8586f68970d38d78dce0
3b00d09 <bd315977@gmail.com>
2024-07-17 06:41:25 +0100

    added scroll to bottom after htmx swap for messages

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,13 @@ document.querySelector("body").addEventListener("htmx:beforeSwap", (e)=>{
   }
 })
 
+// scroll to end after a swap in the messages container, cant make htmx scroll bottom work
+document.body.addEventListener("htmx:oobAfterSwap", (e)=>{
+  if(e.detail.target.id === "messages"){
+    document.querySelector("#messages_container").scrollTo(0, document.querySelector("#messages_container").scrollHeight);
+  }
+})
+
 document.querySelector("#searchBtn").addEventListener("click",()=>{
     document.querySelector("#search-modal").showModal()
 })
